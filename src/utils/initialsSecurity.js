caa3992d0b30bb1afde711e4cc39f348b3ae790a
ferre22mija <117693629+ferre22mija@users.noreply.github.com>
2023-11-16 18:41:12 -0500

    commit cambios por cambiar

--- src/utils/initialsSecurity.js
+++ src/utils/initialsSecurity.js
@@ -1,1 +1,6 @@
 // datos iniciales que deben ingresar en models/seguridad y models/aplicacion
+import { Rol } from "../models/seguridad/Rol";
+
+export const createIncialRoles = async()=>{
+
+}
---
+++ src/utils/initialsValues.js
@@ -0,0 +1,21 @@
+export const roles = [
+    {
+        nombre:"administrador",
+        implicito:true
+    },
+    {
+        nombre:"vendedor",
+        implicito:true
+    }
+]
+
+export const Modulos = [
+    {nombre:"usuario",activo:true},
+    {nombre:"seguridad",activo:true}
+]
+export const Funciones =[
+    
+]
+export const RolesFunciones = [
+
+]
